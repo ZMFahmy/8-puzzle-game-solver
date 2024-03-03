785910593d4483243baeef1bab3671e874c5e191
ziad <ziadmf2019@gmail.com>
2024-03-03 14:39:27 +0200

    continued GUI and solved A* issues

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ p = Puzzle()
 p.shuffle_puzzle()
 
 print("Original state")
+p.set_state(
+    [
+        ["3", "1", "2"],
+        ["0", "4", "7"],
+        ["6", "8", "5"],
+    ]
+)
+
 p.print_puzzle()
-state=[["4","3","1"],
-       ["5","0","2"],
-       ["6","7","8"]]
-p.set_state(state)
 
-visited_nodes = solve_puzzle(p)
+path_to_goal = solve_puzzle(p)
+print("path:")
+print(path_to_goal)
 
-gui = GUI(A_star_states=visited_nodes)
+gui = GUI(A_star_path=path_to_goal)
 gui.show_puzzle_on_screen()
