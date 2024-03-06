c79ec64ff65256d23af5185ca6406aa75c8fc865
Mahmoud El Zamly <elzamlymahmoud250@gmail.com>
2024-03-06 20:45:18 +0200

    ....

--- DFS_Algorithm.py
+++ DFS_Algorithm.py
@@ -177,10 +177,9 @@ def solve_puzzleDFS(puzzle):
             ["3", "4", "5"],
             ["6", "7", "8"],
     ]
-
+    t1=time.time()
     visited_nodes = []
     VisitedSet = set()
-    FrontierSet = set()
 
     frontier = []
     root = DFSNode(puzzle)
@@ -198,6 +197,7 @@ def solve_puzzleDFS(puzzle):
         ch=DFSNode(pu)
         ch.parent=root
         frontier.append(ch)
+
         # pu.print_puzzle()
     solveable=False
     path=[]
@@ -212,6 +212,10 @@ def solve_puzzleDFS(puzzle):
         # print("solved succesfully ")
         # print(visited_nodes[-1].depth)
         current=visited_nodes[-1]
+        t2=time.time()
+        dddepth=current.depth
+        print("time taken for DFS = ",t2-t1)
+        print("the height is ", dddepth)
         # for i in range(0,len(visited_nodes)):
         #     dep=0
         #     l=[]
